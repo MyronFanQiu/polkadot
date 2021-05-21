c444d6c0af075f3a2324bee8d37c82b992ad6132
MyronFanQiu <qiufan_glamour@hotmail.com>
2021-05-21 10:36:32 +0800

    add sudo into release

--- node/service/src/chain_spec.rs
+++ node/service/src/chain_spec.rs
@@ -695,6 +695,7 @@ fn kusama_staging_testnet_config_genesis(wasm_binary: &[u8]) -> kusama::GenesisC
 		},
 		pallet_gilt: Default::default(),
 		parachains_paras: Default::default(),
+		pallet_sudo: kusama::SudoConfig { key: endowed_accounts[0].clone() },
 	}
 }
 
@@ -1220,7 +1221,7 @@ pub fn kusama_testnet_genesis(
 		AssignmentId,
 		AuthorityDiscoveryId,
 	)>,
-	_root_key: AccountId,
+	root_key: AccountId,
 	endowed_accounts: Option<Vec<AccountId>>,
 ) -> kusama::GenesisConfig {
 	let endowed_accounts: Vec<AccountId> = endowed_accounts.unwrap_or_else(testnet_accounts);
@@ -1307,6 +1308,7 @@ pub fn kusama_testnet_genesis(
 		},
 		pallet_gilt: Default::default(),
 		parachains_paras: Default::default(),
+		pallet_sudo: kusama::SudoConfig { key: root_key },
 	}
 }
 
